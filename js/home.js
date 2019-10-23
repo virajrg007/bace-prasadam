79f8de6442a917ba9df123f495dd47414a69baf8
VirajRG <viraj.gawde123@gmail.com>
2019-10-23 16:41:23 +0530

    signup done, meal done partially

---
+++ js/home.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  var user = JSON.parse(localStorage.getItem("username"));
+  console.log(user);
+  $("#name").text(user.username);
+  // user.count.props((num,id)=>{
+  //   if(num==0)
+  //     $("[name="+id+"]").parent().removeClass("active");
+  //   else
+  //     $("[name="+id+"]").parent().addClass("active");
+  // })
+  // $(c1.parent).addClass("active");
+  $(".cb-value").click(function() {
+    var mainParent = $(this).parent(".toggle-btn");
+    var mealNo = $(this).attr("name");
+    if ($(mainParent).find("input.cb-value").is(":checked")) var mealStatus = 1;
+    else var mealStatus = 0;
+    $.ajax({
+      url: "https://tranquil-stream-91687.herokuapp.com/v1/meal",
+      type: "get",
+      data: {
+        username: user.username,
+        mealNo: mealNo,
+        mealStatus: mealStatus
+      },
+      success: function(response) {
+        if (mealStatus==1) {
+          $(mainParent).addClass("active");
+        } else {
+          $(mainParent).removeClass("active");
+        }
+      },
+      error: function(xhr) {
+        alert("login unsuccessful!!");
+      }
+    });
+    
+    console.log(mealNo, mealStatus);
+  });
+
+  //   $("#login-btn").click(function() {
+  //     $.ajax({
+  //       url: "https://tranquil-stream-91687.herokuapp.com/v1/login",
+  //       type: "get", //send it through get method
+  //       data: {
+  //         username: $("#username").val(),
+  //         pass: $("#pass").val()
+  //       },
+  //       success: function(response) {
+  //         localStorage.setItem("username", response);
+  //         setTimeout(()=>window.location.replace("./home.html"), 2000);
+  //       },
+  //       error: function(xhr) {
+  //         alert("login unsuccessful!!");
+  //       }
+  //     });
+  //   });
+});
--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(this).hide();
-  });
   $("#login-btn").click(function() {
     $.ajax({
-        url: "https://tranquil-stream-91687.herokuapp.com/v1/login",
-        type: "get", //send it through get method
-        data: { 
-          username:$("#username").val(),
-          pass:$("#pass").val()
-        },
-        success: function(response) {
-          console.log("login successful!!");
-          window.location.replace("./home.html");
-        },
-        error: function(xhr) {
-          alert("login unsuccessful!!")
-        }
-      });
+      url: "https://tranquil-stream-91687.herokuapp.com/v1/login",
+      type: "get", //send it through get method
+      data: {
+        username: $("#username").val(),
+        pass: $("#pass").val()
+      },
+      success: function(response) {
+        localStorage.setItem("username", response);
+        setTimeout(()=>window.location.replace("./home.html"), 2000);
+      },
+      error: function(xhr) {
+        alert("login unsuccessful!!");
+      }
+    });
   });
 });
